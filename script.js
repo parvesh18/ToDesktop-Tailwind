3d9c27c056c6ed5bce5002259356b434c7c3ae5c
Parvesh Yadav <montyyadav123789@gmail.com>
2024-08-29 12:39:56 +0530

    Tailwind set-up + Responsive Navbar

---
+++ script.js
@@ -0,0 +1,4 @@
+function handleMenu(){
+    const navDialog = document.getElementById('nav-dialog');
+    navDialog.classList.toggle('hidden');
+}
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
